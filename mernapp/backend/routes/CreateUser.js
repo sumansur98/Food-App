9f988bbd8ba2f7d6a5836ed62aeabb79088570ce
Suman Sur <sumansur.cs98@gmail.com>
2024-05-30 10:59:09 +0530

    used express validator on user

--- mernapp/backend/routes/CreateUser.js
+++ mernapp/backend/routes/CreateUser.js
@@ -1,22 +1,37 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
+const { body, validationResult } = require('express-validator');
 
 
-router.post('/createuser', async (req, res)=>{
-    try {
-        await User.create({
-            name : 'suman',
-            email : 'email',
-            password : '123',
-            location : 'kolkata'
-        })
+router.post('/createuser', [
+    body('email').isEmail(),
+    body('password').isLength({ min: 5 })
+],
+    async (req, res) => {
 
-        res.json({success : true})
-    } catch (error) {
-        console.log('error while creating user');
-        res.json({success : true})
-    }
-})
+        console.log(req.body);
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            console.log('validation errors')
+            return res.status(400).json({ errors: errors.array() });
+
+        }
+
+        try {
+            await User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+                location: req.body.location
+            })
+
+            res.json({ success: true })
+        } catch (error) {
+            console.log('error while creating user');
+            res.json({ success: false })
+        }
+    })
 
 module.exports = router;
